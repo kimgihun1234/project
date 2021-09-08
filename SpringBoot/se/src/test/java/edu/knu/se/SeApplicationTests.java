7ed6c5a47d28fd2b0a48b566a3c911c9cc95b5f0
lamknh <of.kimna@gmail.com>
2021-09-08 23:36:23 +0900

    Spring Setup

---
+++ SpringBoot/se/src/test/java/edu/knu/se/SeApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.knu.se;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
