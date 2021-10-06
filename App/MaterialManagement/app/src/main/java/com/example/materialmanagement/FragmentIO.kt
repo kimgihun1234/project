60f9c07e741f28dc24ee1a23cd49804bde5981d9
lamknh <of.kimna@gmail.com>
2021-10-07 00:29:23 +0900

    입출고 UI Search View, Material Toggle Button 작업

--- App/MaterialManagement/app/src/main/java/com/example/materialmanagement/FragmentIO.kt
+++ App/MaterialManagement/app/src/main/java/com/example/materialmanagement/FragmentIO.kt
@@ -1,10 +1,17 @@
 package com.example.materialmanagement
 
+import android.content.res.ColorStateList
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.widget.SearchView
+import androidx.core.content.ContextCompat
+import com.google.android.material.button.MaterialButtonToggleGroup
+
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
@@ -20,6 +27,14 @@ class FragmentIO : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
+    private lateinit var toggleButton : MaterialButtonToggleGroup
+    private lateinit var btnIn : Button
+    private lateinit var btnOut : Button
+    private lateinit var searchOrder : SearchView
+    private lateinit var searchCustomer : SearchView
+    private lateinit var searchStorage : SearchView
+    private lateinit var searchBarCode : SearchView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -32,6 +47,109 @@ class FragmentIO : Fragment() {
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_i_o, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        toggleButton = view.findViewById(R.id.toggleButton)
+        btnIn = view.findViewById(R.id.btnIn)
+        btnOut = view.findViewById(R.id.btnOut)
+
+        toggleButton.addOnButtonCheckedListener{ toggleButton, checkedId, isChecked ->
+            if(isChecked) {
+                when (checkedId) {
+                    R.id.btnIn -> {
+                        Toast.makeText(activity,"입고", Toast.LENGTH_SHORT).show()
+                        btnIn.getBackground().setTint(view.getResources().getColor(R.color.white));
+                        btnOut.getBackground().setTint(view.getResources().getColor(R.color.darkGray));
+                    }
+                    R.id.btnOut -> {
+                        Toast.makeText(activity,"출고", Toast.LENGTH_SHORT).show()
+                        btnOut.getBackground().setTint(view.getResources().getColor(R.color.white));
+                        btnIn.getBackground().setTint(view.getResources().getColor(R.color.darkGray));
+                    }
+                }
+            } else {
+                if(toggleButton.checkedButtonId == View.NO_ID) {
+                    Toast.makeText(activity,"선택 사항 없", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
+        searchOrder = view.findViewById(R.id.searchOrder)
+        searchCustomer = view.findViewById(R.id.searchCustomer)
+        searchStorage = view.findViewById(R.id.searchStorage)
+        searchBarCode = view.findViewById(R.id.searchBarCode)
+
+        searchOrder.isSubmitButtonEnabled = true
+        searchCustomer.isSubmitButtonEnabled = true
+        searchStorage.isSubmitButtonEnabled = true
+        searchBarCode.isSubmitButtonEnabled = true
+
+        searchOrder.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+
+                // 검색 버튼 누를 때 호출
+
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+
+                // 검색창에서 글자가 변경이 일어날 때마다 호출
+
+                return true
+            }
+        })
+
+        searchCustomer.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+
+                // 검색 버튼 누를 때 호출
+
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+
+                // 검색창에서 글자가 변경이 일어날 때마다 호출
+
+                return true
+            }
+        })
+
+        searchStorage.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+
+                // 검색 버튼 누를 때 호출
+
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+
+                // 검색창에서 글자가 변경이 일어날 때마다 호출
+
+                return true
+            }
+        })
+
+        searchBarCode.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+
+                // 검색 버튼 누를 때 호출
+
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+
+                // 검색창에서 글자가 변경이 일어날 때마다 호출
+
+                return true
+            }
+        })
     }
 
     companion object {
