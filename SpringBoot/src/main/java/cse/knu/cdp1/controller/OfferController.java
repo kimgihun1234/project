91da036b7124bb908ef1fbc81978fab8cb5b7401
Dongyeong Kwon <join262@naver.com>
2021-11-09 18:39:49 +0900

    Controller Class 추가 및 수정
    누락된 파일 추가

--- SpringBoot/src/main/java/cse/knu/cdp1/controller/OfferController.java
+++ SpringBoot/src/main/java/cse/knu/cdp1/controller/OfferController.java
@@ -1,9 +1,9 @@
 package cse.knu.cdp1.controller;
 
 import cse.knu.cdp1.dto.CustomerDTO;
-import cse.knu.cdp1.dto.OrderDTO;
+import cse.knu.cdp1.dto.OfferDTO;
 import cse.knu.cdp1.service.CustomerService;
-import cse.knu.cdp1.service.OrderService;
+import cse.knu.cdp1.service.OfferService;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -16,40 +16,41 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-public class OrderController {
+public class OfferController {
     @Autowired
-    OrderService orderService;
+    OfferService offerService;
 
     @Autowired
     CustomerService customerService;
 
-    @Alias("orderresult")
+    @Alias("offerresult")
     @Getter
     @Setter
     @ToString
     public class ResultClass {
-        String plord_no;
+        String ex_requ_no;
         String cust_nm;
         String cust_cd;
     }
 
-    @GetMapping("/orderList")
-    public List<ResultClass> orderReturn() {
+    @GetMapping("/offerList")
+    public List<ResultClass> offerReturn() {
         ArrayList<ResultClass> result = new ArrayList<>();
         CustomerDTO customerInfo;
 
-        List<OrderDTO> orderList = orderService.orderList();
+        List<OfferDTO> offerList = offerService.offerList();
 
-        for(OrderDTO orderData : orderList) {
-            customerInfo = customerService.getCustomerInfo(orderData.getCust_cd());
+        for(OfferDTO offerData : offerList) {
+            customerInfo = customerService.getCustomerInfo(offerData.getCust_cd());
             ResultClass temp = new ResultClass();
-            temp.plord_no = orderData.getPlord_no();
+            temp.ex_requ_no = offerData.getEx_requ_no();
             temp.cust_nm = customerInfo.getCust_nm();
-            temp.cust_cd = orderData.getCust_cd();
+            temp.cust_cd = offerData.getCust_cd();
             result.add(temp);
         }
 
-        /* 발주번호/거래처이름/거래처번호 */
+        /* 출고요청번호/거래처이름/거래처번호 */
         return result;
     }
+
 }
